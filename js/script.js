c4f560d7292d077326dfaca5a6a498184a942ea8
bihong <hkh7208@naver.com>
2026-01-04 09:01:24 +0900

    frame 작성

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// script.js: JavaScript 동작 정의
+
+// DOMContentLoaded 이벤트 리스너
+// 페이지가 로드된 후 실행될 스크립트를 여기에 작성합니다.
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("페이지가 로드되었습니다.");
+});
